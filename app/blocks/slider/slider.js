ac385bb45c12b3ec90900b11c74a5fe1def379aa
Gleb Gorokhov <gg@verstaburo.com>
2020-02-25 15:21:47 +0300

    Правки

--- app/blocks/slider/slider.js
+++ app/blocks/slider/slider.js
@@ -22,7 +22,7 @@ export function slider() {
 
       $.each(objects, function (i) {
         // Генерируем многострочный заголовок
-        function createTitle () {
+        function createTitle() {
           let title = '';
 
           $.each(objects[i].title, function (n) {
@@ -51,69 +51,90 @@ export function slider() {
         slidesContent.append(html);
       });
 
-      window.mySlider = new Swiper(block, {
-        loop: false,
-        speed: 1000,
-        autoplay: {
-          delay: 10500,
-          disableOnInteraction: false,
-          waitForTransition: false,
-        },
-        slidesPerView: 1,
-        spaceBetween: 0,
-        centeredSlides: false,
-        roundLengths: true,
-        effect: 'fade',
-        freeMode: false,
-        keyboard: {
-          enabled: true,
-          onlyInViewport: true,
-        },
-        mousewheel: {
-          releaseOnEdges: true,
-        },
-        pagination: {
-          el: block.find('.circle-dots'),
-          clickable: true,
-          bulletActiveClass: 'is-active',
-          bulletClass: 'circle-dots__circle',
-          renderBullet: function (index, className) {
-            return `
-            <div class="${className}">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
-                    <circle class="js-circle-dot-percent" cx="16" cy="16" r="15" style="transition: stroke-dashoffset ${this.params.autoplay.delay}ms linear;"></circle>
-                </svg>
-                <div class="circle-dots__clickable"></div>
-            </div>
-          `;
+      const mainVideo = $(document).find(`.slider__video:eq(0) video`)[0];
+
+      let sliderTimer;
+
+      mainVideo.addEventListener('loadedmetadata', function () {
+        window.mySlider = new Swiper(block, {
+          loop: false,
+          speed: 1000,
+          autoplay: {
+            delay: 105000000000000,
+            disableOnInteraction: false,
+            waitForTransition: false,
           },
-        },
-        on: {
-          init: function () {
-            this.slideTo(1);
+          slidesPerView: 1,
+          spaceBetween: 0,
+          centeredSlides: false,
+          roundLengths: true,
+          effect: 'fade',
+          freeMode: false,
+          keyboard: {
+            enabled: true,
+            onlyInViewport: true,
+          },
+          mousewheel: {
+            releaseOnEdges: true,
+          },
+          pagination: {
+            el: block.find('.circle-dots'),
+            clickable: true,
+            bulletActiveClass: 'is-active',
+            bulletClass: 'circle-dots__circle',
+            renderBullet: function (index, className) {
+              const video = $(document).find(`.slider__video:eq(${index}) video`)[0];
 
-            document.onkeydown = checkKey;
+              return `
+                <div class="${className}">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+                        <circle class="js-circle-dot-percent" cx="16" cy="16" r="15" style="transition: stroke-dashoffset ${video.duration*1000 + 500}ms linear;"></circle>
+                    </svg>
+                    <div class="circle-dots__clickable"></div>
+                </div>
+              `;
+            },
+          },
+          on: {
+            init: function () {
+              this.slideTo(1);
 
-            const sw = this;
+              document.onkeydown = checkKey;
 
-            function checkKey(e) {
-              e = e || window.event;
+              const sw = this;
 
-              if (e.keyCode == '38') {
-                sw.slidePrev();
-              } else if (e.keyCode == '40') {
-                sw.slideNext();
+              function checkKey(e) {
+                e = e || window.event;
+
+                if (e.keyCode == '38') {
+                  sw.slidePrev();
+                } else if (e.keyCode == '40') {
+                  sw.slideNext();
+                }
               }
-            }
-          },
-          slideChangeTransitionStart: function () {
-            $(document).find('.swiper-slide-active video')[0].currentTime = 0;
+            },
+            slideChangeTransitionStart: function () {
+              const thisVideo = $(document).find('.swiper-slide-active video')[0];
+              thisVideo.currentTime = 0;
 
-            setTimeout(() => {
-              $(document).find('.swiper-slide-active video')[0].play();
-            }, 500);
+              setTimeout(() => {
+                thisVideo.play();
+              }, 500);
+
+              clearTimeout(sliderTimer);
+
+              const info = this;
+
+              sliderTimer = setTimeout(() => {
+                if (info.isEnd) {
+                  mySlider.slideTo(0);
+                } else {
+                  mySlider.slideNext();
+                }
+              }, thisVideo.duration*1000 + 500);
+            },
           },
-        },
+        });
       });
     });
   });
